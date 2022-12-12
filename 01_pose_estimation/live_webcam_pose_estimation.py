2d4832e4cd72cda80e3dd2e2c061b977cd6be73f
schauppi <46676413+schauppi@users.noreply.github.com>
2022-12-12 14:55:30 +0100

    fixed memory consumtion .... probably?

--- 01_pose_estimation/live_webcam_pose_estimation.py
+++ 01_pose_estimation/live_webcam_pose_estimation.py
@@ -5,6 +5,8 @@ from utils.datasets import letterbox
 from utils.general import non_max_suppression_kpt
 from utils.plots import output_to_keypoint, plot_skeleton_kpts
 
+from models.experimental import attempt_load
+
 import matplotlib.pyplot as plt
 import cv2
 import numpy as np
@@ -14,69 +16,76 @@ import time
 import torch
 import math
 
+device = "cpu"
+
 def load_model():
-    #device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
-    device = torch.device("cpu")
-    model = torch.load('yolov7-w6-pose.pt', map_location=device)['model']
-    # Put in inference mode
-    model.float().eval()
-    return model
+    try:
+        model = attempt_load('model_weights/yolov7-w6-pose.pt', map_location=device)
+        model.eval()
+        print("Model loaded successfully")
+        return model
+    except:
+        print("Model not found")
 
-model = load_model()
+def predict(image, model):
+    with torch.inference_mode():
+        pred = model(image[None], augment=False)[0]
 
-def run_inference(frame, model):
-    # Resize and pad frame
-    frame = letterbox(frame, 960, stride=64, auto=True)[0] # shape: (768, 960, 3)
-    # Apply transforms
-    frame = transforms.ToTensor()(frame) # torch.Size([3, 768, 960])
-    # Turn frame into batch
-    frame = frame.unsqueeze(0) # torch.Size([1, 3, 768, 960])
-    output, _ = model(frame) # torch.Size([1, 45900, 57])
-    return output, frame
+    prediction = non_max_suppression_kpt(pred, 0.25, 0.65, nc=model.yaml['nc'], nkpt=model.yaml['nkpt'], kpt_label=True)
+    prediction = output_to_keypoint(prediction)
 
-def show_pose_estimated_video(output, image):
-    output = non_max_suppression_kpt(output, 
-                                     0.25, # Confidence Threshold
-                                     0.65, # IoU Threshold
-                                     nc=model.yaml['nc'], # Number of Classes
-                                     nkpt=model.yaml['nkpt'], # Number of Keypoints
-                                     kpt_label=True)
-    with torch.no_grad():
-        output = output_to_keypoint(output)
-    nimg = image[0].permute(1, 2, 0) * 255
-    nimg = nimg.cpu().numpy().astype(np.uint8)
-    nimg = cv2.cvtColor(nimg, cv2.COLOR_RGB2BGR)
-    
-    for idx in range(output.shape[0]):
-        plot_skeleton_kpts(nimg, output[idx, 7:].T, 3)
-    
-    cv2.imshow('frame', nimg)
+    if len(prediction) > 0:
+        prediction = prediction[:, 7:]
+        return prediction
+    else:
+        return None
 
+def prepare_image(frame, image_size=640):
+    image = np.asarray(frame)
 
-# define a video capture object
-vid = cv2.VideoCapture(0)
-cv2.startWindowThread()
+    original_height, original_width = image.shape[:2]
+    image = cv2.resize(image, (image_size, image_size))
 
-counter = 1 
-while(True):
-	
-	# Capture the video frame
-	# by frame
-    ret, frame = vid.read()
-    
-    # take a picture approx. every sec
-    if ret and counter%60==0:
-        output, image = run_inference(frame, model)
-        show_pose_estimated_video(output, image)
-        
-    counter = counter + 1
-	# the 'q' button is set as the
-	# quitting button you may use any
-	# desired button of your choice
+    image_pt = torch.from_numpy(image).permute(2, 0, 1).to(device)
+    image_pt = image_pt.float() / 255.0
+
+    return image_pt, original_height, original_width
+
+def resize_pose_estimation_prediction_output(prediction, original_height, original_width, image_size=640):
+    prediction[:, 0::3] *= original_width / image_size
+    prediction[:, 1::3] *= original_height / image_size
+
+    return prediction
+
+def plot_pose(prediction, image):
+    image = np.asarray(image)
+
+    for pred in range(prediction.shape[0]):
+        plot_skeleton_kpts(image, prediction[pred].T, 3)
+
+    return image
+
+def detect_poses(frame, model):
+    image, original_height, original_width = prepare_image(frame)
+    prediction = predict(image, model)
+    if prediction is not None:
+        prediction = resize_pose_estimation_prediction_output(prediction, original_height, original_width)
+        image_with_poses = plot_pose(prediction, frame)
+        return image_with_poses
+    else:
+        return frame
+
+pose_estimation_model = load_model()
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if ret:
+        frame = detect_poses(frame, pose_estimation_model)
+        cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# After the loop release the cap object
-vid.release()
-# Destroy all the windows
+cap.release()
 cv2.destroyAllWindows()
